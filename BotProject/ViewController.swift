4cc28097720552714c563f20ca274f2196afaceb
James Rickett <jrickett@twopicode.com.au>
2019-07-03 17:58:04 +1000

    Initial Commit

---
+++ BotProject/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  BotProject
+//
+//  Created by James Rickett on 3/7/19.
+//  Copyright © 2019 James Rickett. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+    }
+
+
+}
+
